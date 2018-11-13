de534f17f8c632db9a907a714c2c0e17290348a4
maduar <maduar86@gmail.com>
2018-11-13 22:28:18 +0800

    add index

--- src/main/java/com/madaur/springboot2/App.java
+++ src/main/java/com/madaur/springboot2/App.java
@@ -2,12 +2,23 @@ package com.madaur.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
+@RestController
 public class App {
+
+
+    @RequestMapping("index")
+    public String test() {
+        return "test";
+    }
+
     public static void main(String[] args) {
 
         SpringApplication.run(App.class, args);
