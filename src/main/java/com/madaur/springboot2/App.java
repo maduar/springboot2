6b78331aeddd29a43d79e1b914781a27c9539e82
maduar <maduar86@gmail.com>
2019-03-13 13:58:29 +0800

    add nacos

--- src/main/java/com/madaur/springboot2/App.java
+++ src/main/java/com/madaur/springboot2/App.java
@@ -1,26 +1,16 @@
 package com.madaur.springboot2;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-@RestController
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
 public class App {
-
-
-    @RequestMapping("index")
-    public String test() {
-        return "test";
-    }
 
     public static void main(String[] args) {
 
