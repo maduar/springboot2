82ef90088ee843c69bf1dc0f3f07c92751f89a9b
maduar <maduar86@gmail.com>
2019-04-01 15:25:13 +0800

    add spring.application.name

--- src/main/java/com/madaur/springboot2/MyWatcher.java
+++ src/main/java/com/madaur/springboot2/MyWatcher.java
@@ -1,17 +1,17 @@
-package com.madaur.springboot2;
-
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-
-/**
- * Description:
- *
- * @author maduar maduar@163.com
- * @version 1.1.1 14/03/2019
- */
-public class MyWatcher implements Watcher {
-    @Override
-    public void process(WatchedEvent watchedEvent) {
-        System.out.println("WatchedEvent: " + watchedEvent);
-    }
-}
+//package com.madaur.springboot2;
+//
+//import org.apache.zookeeper.WatchedEvent;
+//import org.apache.zookeeper.Watcher;
+//
+///**
+// * Description:
+// *
+// * @author maduar maduar@163.com
+// * @version 1.1.1 14/03/2019
+// */
+//public class MyWatcher implements Watcher {
+//    @Override
+//    public void process(WatchedEvent watchedEvent) {
+//        System.out.println("WatchedEvent: " + watchedEvent);
+//    }
+//}
